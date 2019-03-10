82a792ec0b5d4fc996e7514e9c79cd1c59bfd84c
Stephen Catsamas <seca8193@gmail.com>
2019-03-10 22:37:52 +1100

    Added servo library and basic info on how to use

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,3 +1,11 @@
+/*
+Main arduino code file for Meme_AI_Turret
+Contributors: Stephen Catsmas
+              Boris Deletic
+*/
+
+#include <Servo.h>
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -7,3 +15,17 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+
+
+/*
+notes on how to use (Servo.h)
+to create servo object:
+Servo foo;
+attached servo variable to pin:
+foo.attach(pin);
+to position servo:
+foo.write(deg);
+read servo position (read out in deg):
+foo.read();
